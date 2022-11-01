4c275aefe63734c98929da73dd40b22c6075b64a
Noah Fajarda <noahfajarda@gmail.com>
2022-11-01 09:47:58 -0700

    simplified algorithm (military time)

--- script.js
+++ script.js
@@ -10,39 +10,29 @@ var retrievedText;
 var textUponSave = $('#textUponSave');
 
 // initiate list of hours
-var hourList = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+var hourList = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 // Set text of current day
 currentDay.text(today.format("dddd, MMMM Do, YYYY"));
 
+// WE DOING MILITARY TIME
 for (var i in hourList) {
     // get the current hour element
     hourEl = $('#' + hourList[i]);
-    
-    // convert today's hour to military time
-    if (hourToday <= 9) {
-        hourToday += 12;
-    }
-    // convert iterated hour to military time
-    if (hourList[i] < 9) {
-        hourList[i] += 12;
-    }
-    
+
     // check if hour equals the element
     if (hourToday == hourList[i]) {
         hourEl.addClass('present');
     } else if (hourToday >= hourList[i]) {
-        hourEl.addClass('past');
-    } else {
         hourEl.addClass('future');
+    } else {
+        hourEl.addClass('past');
     }
 
-    if (hourList[i] > 12) {
-        hourList[i] -= 12;
-    }
+    // set text upon reload
     retrievedText = localStorage.getItem("time-" + hourList[i]);
     hourEl.text(retrievedText);
-};
+}
 
 // event listener for all elements
 saveButtons.on('click', function(event) {
@@ -54,15 +44,13 @@ saveButtons.on('click', function(event) {
     localStorage.setItem("time-" + elementID, currentTextArea.val());
 
     // display 'SAVED' text in header upon save
-    textUponSave.text('SAVED!');
-    textUponSave.css('color', 'red');
+    textUponSave.text('SAVED!').css('color', 'red');
     var secondsLeft = 1;
     var timerInterval = setInterval(function() {
         secondsLeft--;
         if (secondsLeft === 0) {
             clearInterval(timerInterval);
-            textUponSave.css('color', 'white');
-            textUponSave.text('.');
+            textUponSave.css('color', 'white').text('.');
         }
     }, 1000);
 });
